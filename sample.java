482f5efc553bacd3daa44f604ff9b17d46807c76
Sathiya Arumugam <86502016+SathiyaManimaran@users.noreply.github.com>
2023-05-28 10:08:05 -0400

    addednewline

---
+++ sample.java
@@ -0,0 +1,13 @@
+import java io.*;
+
+public class sample
+{
+	public static void main(String args[])
+		{
+
+			System.out.println("Hello world 12345 !!");
+			System.out.println("hellow world");
+		}
+}
+
+
